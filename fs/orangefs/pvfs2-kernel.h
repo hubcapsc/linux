f9268a3fdc504c47edc167448fb2a230d46e79ec
Mike Marshall <hubcap@omnibond.com>
2014-06-26 11:06:11 -0400

    Orangefs: XOR all bits from uuid into resulting inode number.
    Signed-off-by: Mike Marshall <hubcap@omnibond.com>

--- fs/orangefs/pvfs2-kernel.h
+++ fs/orangefs/pvfs2-kernel.h
@@ -463,15 +463,14 @@ static inline ino_t pvfs2_khandle_to_ino(PVFS_khandle *khandle)
 		uint64_t ino;
 	} ihandle;
 
-
-	ihandle.u[0] = khandle->u[0];
-	ihandle.u[1] = khandle->u[1];
-	ihandle.u[2] = khandle->u[2];
-	ihandle.u[3] = khandle->u[3];
-	ihandle.u[4] = khandle->u[12];
-	ihandle.u[5] = khandle->u[13];
-	ihandle.u[6] = khandle->u[14];
-	ihandle.u[7] = khandle->u[15];
+	ihandle.u[0] = khandle->u[0] ^ khandle->u[4];
+	ihandle.u[1] = khandle->u[1] ^ khandle->u[5];
+	ihandle.u[2] = khandle->u[2] ^ khandle->u[6];
+	ihandle.u[3] = khandle->u[3] ^ khandle->u[7];
+	ihandle.u[4] = khandle->u[12] ^ khandle->u[8];
+	ihandle.u[5] = khandle->u[13] ^ khandle->u[9];
+	ihandle.u[6] = khandle->u[14] ^ khandle->u[10];
+	ihandle.u[7] = khandle->u[15] ^ khandle->u[11];
 
 	return ihandle.ino;
 }
