30f513f2e7fe093b8f405b221653fa5a28e77f26
Christoph Hellwig <hch@lst.de>
2014-06-30 16:10:50 +0200

    orangefs: use generic_readlink
    And also clean up the follow link code a little bit while at it.
    
    Signed-off-by: Mike Marshall <hubcap@omnibond.com>

--- fs/orangefs/symlink.c
+++ fs/orangefs/symlink.c
@@ -8,44 +8,20 @@
 #include "pvfs2-kernel.h"
 #include "pvfs2-bufmap.h"
 
-static int pvfs2_readlink(struct dentry *dentry,
-			  char __user *buffer,
-			  int buflen)
-{
-	pvfs2_inode_t *pvfs2_inode = PVFS2_I(dentry->d_inode);
-
-	gossip_debug(GOSSIP_INODE_DEBUG,
-		     "pvfs2_readlink called on inode %pU\n",
-		     get_khandle_from_ino(dentry->d_inode));
-
-	/*
-	 * if we're getting called, the vfs has no doubt already done a
-	 * getattr, so we should always have the link_target string
-	 * available in the pvfs2_inode private data
-	 */
-	return vfs_readlink(dentry, buffer, buflen, pvfs2_inode->link_target);
-}
-
 static void *pvfs2_follow_link(struct dentry *dentry, struct nameidata *nd)
 {
-	pvfs2_inode_t *pvfs2_inode = PVFS2_I(dentry->d_inode);
+	char *target =  PVFS2_I(dentry->d_inode)->link_target;
 
 	gossip_debug(GOSSIP_INODE_DEBUG,
-		     "pvfs2: pvfs2_follow_link called on %s (target is %p)\n",
-		     (char *)dentry->d_name.name,
-		     pvfs2_inode->link_target);
+		     "pvfs2: %s called on %s (target is %p)\n",
+		     __func__, (char *)dentry->d_name.name, target);
 
-	/*
-	 * we used to use vfs_follow_link here, instead of nd_set_link.
-	 * vfs_follow_link had an int return value, nd_set_link does not,
-	 * so now we'll just call nd_set_link and return NULL...
-	 */
-	nd_set_link(nd, pvfs2_inode->link_target);
+	nd_set_link(nd, target);
 	return NULL;
 }
 
 struct inode_operations pvfs2_symlink_inode_operations = {
-	.readlink = pvfs2_readlink,
+	.readlink = generic_readlink,
 	.follow_link = pvfs2_follow_link,
 	.setattr = pvfs2_setattr,
 	.getattr = pvfs2_getattr,
